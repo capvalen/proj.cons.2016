a710c942d6264bb33ea6f673f0f678025a16f144
Infocat <infocat2.0@gmail.com>
2021-04-09 21:01:25 -0500

    1.9.3

--- piePagina.php
+++ piePagina.php
@@ -1,11 +1,11 @@
 <footer style="font-size: 12px;">
 	<div class="footer container-fluid noselect hidden-print" >
 	<div class="col-xs-3 visible-print-block"><img src="images/logoInfocat.svg" class="img-responsive" alt=""></div>
-	<div class="col-xs-12 hidden-print col-sm-3 col-sm-offset-2 text-right"><img src="images/logoInfocat.svg" class="img-responsive" alt=""></div>
+	<div class="col-xs-12 hidden-print col-sm-3 col-sm-offset-2 "><center><img src="images/logoInfocat.svg" class="img-responsive" style="max-width: 60%;"></center></div>
 	<div class="col-xs-12 col-sm-6 text-left"><p>
 		<span><strong>Desarrollado para:</strong> Consultorio O.R.L. - Huancayo</span><br>
 		<span><strong>Por:</strong> Infocat Soluciones S.A.C. 2016-<?php echo date("Y"); ?> </span><br>
-		<span><strong>Web:</strong> <a href="https://fb.com/infocatsoluciones" style="color: #f3ceff;">fb.com/infocatsoluciones</a> </span> | <span><strong>Whastapp:</strong> <a href="https://wa.me/51977692108?text=Ayuda con el sistema de ORL" target="_blank" style="color: #f3ceff;">977-692108</a> </span><br>
+		<span><strong>Web:</strong> <a href="https://fb.com/infocatsoluciones" style="color: #f3ceff;"z>fb.com/infocatsoluciones</a> </span> | <span><strong>Whastapp:</strong> <a href="https://wa.me/51977692108?text=Ayuda con el sistema de ORL" target="_blank" style="color: #f3ceff;">977-692108</a> </span><br>
 		<span><strong>Versión:</strong> <?php include 'php/version.php' ?> </span>
 	</p>
 	</div>
